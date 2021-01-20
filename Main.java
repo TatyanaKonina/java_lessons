c687d2f08a5769760626246e0fdc14c1013ff1d7
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-20 23:03:21 +0300

    if/else/switch

--- Main.java
+++ Main.java
@@ -3,14 +3,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner num = new Scanner(System.in);
-        int first,second,result;
-        System.out.print("first num : ");
+        int first, second = 50;
+        boolean isTrue = true;
+        System.out.print("enter num: ");
         first = num.nextInt();
-        System.out.print("second num : ");
-        second = num.nextInt();
 
-        result = first + second;
-        result ++;
-        System.out.println(result);
+        isTrue = first == 51 ? true : false;
+
+        System.out.println(isTrue);
+
+        if (first > 50 || isTrue) 
+            System.out.println("more than 50");
+        else {
+            System.out.println("lessl");
+        }
+
+        switch(first){
+            case 51: 
+                System.out.println("== 51");
+                break;
+            case 55 : {
+                System.out.println(" == 55");
+                break;
+            }
+            default : {
+                System.out.println(first);
+            }
+        }
+        
     }
 }
