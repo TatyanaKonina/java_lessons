1b344544ed0b23d6b93bd46f639667f244226713
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-20 22:23:05 +0300

    variables

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.lang.String;
+
+public class Main {
+    public static void main(String[] args){
+        byte num = 12;
+        int num2 = 15616;
+        long num3= 555655656;
+
+        float num4 = 23.45f;
+        double num5 = 55.6d;
+
+        boolean isTrue = true;        
+
+        char sym = 's';
+
+        String str = "Hello User";
+
+        System.out.println(str);
+    }
+}
