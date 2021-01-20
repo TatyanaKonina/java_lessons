d3ca1b62618cac8f6acb385aa9db1ff2fcf08d8b
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-20 22:31:41 +0300

    input string

--- Main.java
+++ Main.java
@@ -1,20 +1,10 @@
-import java.lang.String;
+
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        byte num = 12;
-        int num2 = 15616;
-        long num3= 555655656;
-
-        float num4 = 23.45f;
-        double num5 = 55.6d;
-
-        boolean isTrue = true;        
-
-        char sym = 's';
-
-        String str = "Hello User";
-
-        System.out.println(str);
+        Scanner str = new Scanner(System.in);
+        System.out.println("Hello " + str.nextLine());
     }
 }
