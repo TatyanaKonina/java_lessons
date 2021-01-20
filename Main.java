edd5a94fd16f0bce077effb58b1cedf339fb6c03
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-20 22:41:10 +0300

    math oper

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
-
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner str = new Scanner(System.in);
-        System.out.println("Hello " + str.nextLine());
+        Scanner num = new Scanner(System.in);
+        int first,second,result;
+        System.out.print("first num : ");
+        first = num.nextInt();
+        System.out.print("second num : ");
+        second = num.nextInt();
+
+        result = first + second;
+        result ++;
+        System.out.println(result);
     }
 }
