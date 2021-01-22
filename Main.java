d045279c879d517366e62e4ae20caddf69d45776
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-22 12:09:20 +0300

    cycles

--- Main.java
+++ Main.java
@@ -2,34 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner num = new Scanner(System.in);
-        int first, second = 50;
-        boolean isTrue = true;
-        System.out.print("enter num: ");
-        first = num.nextInt();
-
-        isTrue = first == 51 ? true : false;
-
-        System.out.println(isTrue);
-
-        if (first > 50 || isTrue) 
-            System.out.println("more than 50");
-        else {
-            System.out.println("lessl");
-        }
-
-        switch(first){
-            case 51: 
-                System.out.println("== 51");
-                break;
-            case 55 : {
-                System.out.println(" == 55");
-                break;
+        // for(int i = 0;i < 10; ++i){
+        //     System.out.println(i);
+        // // }
+        int j = 0;
+        while (j < 20) {
+            j++;
+            if (j == 4){
+                continue;
             }
-            default : {
-                System.out.println(first);
-            }
+            System.out.println(j);
         }
-        
+        // int x = 0;
+        // do {
+        //     x++;
+        //     System.out.println(x);
+        // }while(x < 10);
     }
 }
