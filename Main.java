8aca79e572960e7c913c8e9bd5c99489426f7834
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-22 12:32:13 +0300

    sorting

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
-
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args){
-        int[]a;
-        int n;
-        Scanner in = new Scanner(System.in);
-        System.out.println("enter n");
-        n = in.nextInt();
-        a = new int [n];
-        for(int i = 0; i<n;i++){
-            System.out.print("enter num");
-            a[i] = in.nextInt();
+        int[] source = new int[10];
+        for(int i=0; i < source.length; i++){
+            source[i] = 10 - i;
         }
-        System.out.println("len: " + a.length);
-
+        // int[] dest = Arrays.copyOf(source, source.length);
+        // for(int i = 0; i < dest.length;i++){
+        //     System.out.println(dest[i]);
+        // }
+        // System.out.println(java.util.Arrays.copyOfRange(source,0,source.length - 1));
+        System.out.println(java.util.Arrays.toString(source));
+        java.util.Arrays.sort(source);
+        System.out.println(java.util.Arrays.toString(source));
     }
 }
