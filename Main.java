a4c87f03440d1abda40df09623570b92a2f9372a
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-22 13:31:10 +0300

    constructors

--- Main.java
+++ Main.java
@@ -2,11 +2,12 @@
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args){
-        Person vitya = new Person();
-        vitya.height = 120;
-        System.out.println( vitya.height);
+        Person vitya = new Person(120,"vitya");
+        System.out.println( vitya.name);
         Person vlad = new Person();
-        System.out.println(vlad.height);
+        System.out.println(vlad.name);
         vitya.say("Geaorge");
+        Person dima = new Person("dIMA");
+        System.out.println(dima.height);
     }
 }
--- Person.java
+++ Person.java
@@ -1,6 +1,18 @@
 public class Person {
-    public int height = 180;
+    public int height;
+    public String name = "Default";
     public void say (String name){
         System.out.println("Hello, " + name);
     }
+    public Person(int h, String n){
+        height = h;
+        name = n;
+    }
+    public Person() {
+        height = 180;
+    }
+    public Person(String n) {
+        name = n;
+    }
+
 }
