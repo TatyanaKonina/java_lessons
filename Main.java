4aec2ccdb46e8f1e8c1de6d3f968b6b984d77402
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-22 13:06:56 +0300

    func

--- Main.java
+++ Main.java
@@ -2,17 +2,15 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args){
-        int[] source = new int[10];
-        for(int i=0; i < source.length; i++){
-            source[i] = 10 - i;
-        }
-        // int[] dest = Arrays.copyOf(source, source.length);
-        // for(int i = 0; i < dest.length;i++){
-        //     System.out.println(dest[i]);
-        // }
-        // System.out.println(java.util.Arrays.copyOfRange(source,0,source.length - 1));
-        System.out.println(java.util.Arrays.toString(source));
-        java.util.Arrays.sort(source);
-        System.out.println(java.util.Arrays.toString(source));
+        System.out.println(func(3,4));
+        //int temp = func(3,4);
     }
+    public static int func (int a,int b) {
+        int result = 1;
+        for (int i = 0; i < b;i++){
+            result *=a;
+        }
+        return result;
+    }
+
 }
