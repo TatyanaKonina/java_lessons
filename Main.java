ddc5f583cded7554ead10c7c5bc62d572b89f690
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-23 21:33:30 +0300

    interface

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[]args){
-        final int b = 11;
-        //b = 55;//error
-        int x = Math.mult(12,54);
-        System.out.println(x);
-        Math a = new Math();
-        System.out.println(a.x);
-        Math.x = 225;
-        System.out.println(a.x);
-        Math a1 = new Math();
-        System.out.println(a1.x);
+        Array a = new MyArray();
+        System.out.println(a.Add(30));
+        Array b = new MySecondArray();
+            
     }
 }
