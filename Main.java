4d47473ba1d0c34245dd137dcc87dd683ffecac0
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-23 20:33:13 +0300

    abstract class

--- Main.java
+++ Main.java
@@ -2,14 +2,7 @@
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args){
-        // Computer comp = new Computer();
-        // comp.i7.start();
-        // comp.transfer.start();
-        new Computer(){
-            void superComp(){
-                this.i7.start();
-                this.transfer.start();
-            }
-        }
+       Shape circle = new Circle();
+       circle.draw();
     }
 }
